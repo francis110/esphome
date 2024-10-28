54bac6c0d365ad91b0a38ee3715a414ae6770011
francis110 <bos.kevin.m@gmail.com>
2024-10-27 21:15:05 -0400

    Update uart_read_line_sensor.h

--- esphome/components/uart_read_line_sensor/uart_read_line_sensor.h
+++ esphome/components/uart_read_line_sensor/uart_read_line_sensor.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/automaton.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
@@ -11,13 +10,12 @@ namespace uart_read_line_sensor {
 class UartReadLineSensor : public Component, public uart::UARTDevice {
  public:
   void setup() override {
-    // Initialize any required resources or settings here
+    // Initialization code, if needed
   }
 
   void loop() override {
-    // Read data from UART and process it here
-    // For example, read a line and publish it to the text sensor
-    if (this->available()) {
+    // Reading data from UART
+    while (this->available()) {
       String line = this->read_line();
       if (!line.isEmpty()) {
         this->publish_line(line);
@@ -26,8 +24,9 @@ class UartReadLineSensor : public Component, public uart::UARTDevice {
   }
 
   void publish_line(const String &line) {
-    // Publish the line to the text sensor
-    this->text_sensor_->publish_state(line);
+    if (this->text_sensor_) {
+      this->text_sensor_->publish_state(line);
+    }
   }
 
   void set_text_sensor(text_sensor::TextSensor *text_sensor) {

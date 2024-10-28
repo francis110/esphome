cc88e31dcf57403229cfb53af49b2e307d34e18b
francis110 <bos.kevin.m@gmail.com>
2024-10-27 20:14:32 -0400

    Update uart_read_line_sensor.h

--- esphome/components/uart_read_line_sensor/uart_read_line_sensor.h
+++ esphome/components/uart_read_line_sensor/uart_read_line_sensor.h
@@ -1,51 +1,26 @@
 #include "esphome.h"
 
-class UartReadLineSensor : public Component, public UARTDevice, public TextSensor {
+class UartReadLineSensor : public Component, public uart::UARTDevice, public Sensor {
  public:
-  UartReadLineSensor(UARTComponent *parent) : UARTDevice(parent) {}
+  UartReadLineSensor(UARTComponent *parent) : uart::UARTDevice(parent) {}
 
   void setup() override {
-    // nothing to do here
-  }
-
-  int readline(int readch, char *buffer, int len)
-  {
-    static int pos = 0;
-    int rpos;
-
-    if (readch > 0) {
-      switch (readch) {
-        case '\n':
-        case '\r': // Return on CR or newline
-          buffer[pos] = 0; // Just to be sure, set last character 0
-          rpos = pos;
-          pos = 0;  // Reset position index ready for next time
-          return rpos;
-        default:
-          if ((pos < len-1) && ( readch < 127 )) { // Filter on <127 to make sure it is a character
-            buffer[pos++] = readch;
-            buffer[pos] = 0;
-          }
-          else
-          {
-            buffer[pos] = 0; // Just to be sure, set last character 0
-            rpos = pos;
-            pos = 0;  // Reset position index ready for next time
-            return rpos;
-          }
-      }
-    }
-    // No end of line has been found, so return -1.
-    return -1;
+    // This will be called by App.setup()
   }
 
   void loop() override {
-    const int max_line_length = 80;
-    static char buffer[max_line_length];
+    // This will be called by App.loop()
     while (available()) {
-      if(readline(read(), buffer, max_line_length) > 0) {
-        publish_state(buffer);
+      char c = read();
+      if (c == '\n') {
+        publish_state(line_.c_str());
+        line_ = "";
+      } else {
+        line_ += c;
       }
     }
   }
+
+ protected:
+  std::string line_;
 };

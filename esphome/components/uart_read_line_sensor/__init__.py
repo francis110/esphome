d5c2a9ecf9e3aac0797c3522959efc6d66878819
francis110 <bos.kevin.m@gmail.com>
2024-10-27 20:11:09 -0400

    Update __init__.py

--- esphome/components/uart_read_line_sensor/__init__.py
+++ esphome/components/uart_read_line_sensor/__init__.py
@@ -1,4 +1,11 @@
-# __init__.py for UartReadLineSensor component
-from esphome import core
+# __init__.py for uart_read_line_sensor component
 
-# Placeholder to make sure the component initializes
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, uart
+import esphome.const as const
+
+DEPENDENCIES = ['uart']
+uart_read_line_sensor_ns = cg.esphome_ns.namespace('uart_read_line_sensor')
+
+CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend({})

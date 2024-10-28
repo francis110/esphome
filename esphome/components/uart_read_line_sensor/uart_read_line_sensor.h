e59e90c8d48454d62ac49dcfec171b33378f3b9d
francis110 <bos.kevin.m@gmail.com>
2024-10-27 21:12:48 -0400

    Update uart_read_line_sensor.h

--- esphome/components/uart_read_line_sensor/uart_read_line_sensor.h
+++ esphome/components/uart_read_line_sensor/uart_read_line_sensor.h
@@ -1,26 +1,42 @@
-#include "esphome.h"
+#pragma once
 
-class UartReadLineSensor : public Component, public uart::UARTDevice, public Sensor {
+#include "esphome/core/component.h"
+#include "esphome/core/automaton.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+
+namespace esphome {
+namespace uart_read_line_sensor {
+
+class UartReadLineSensor : public Component, public uart::UARTDevice {
  public:
-  UartReadLineSensor(UARTComponent *parent) : uart::UARTDevice(parent) {}
-
   void setup() override {
-    // This will be called by App.setup()
+    // Initialize any required resources or settings here
   }
 
   void loop() override {
-    // This will be called by App.loop()
-    while (available()) {
-      char c = read();
-      if (c == '\n') {
-        publish_state(line_.c_str());
-        line_ = "";
-      } else {
-        line_ += c;
+    // Read data from UART and process it here
+    // For example, read a line and publish it to the text sensor
+    if (this->available()) {
+      String line = this->read_line();
+      if (!line.isEmpty()) {
+        this->publish_line(line);
       }
     }
   }
 
+  void publish_line(const String &line) {
+    // Publish the line to the text sensor
+    this->text_sensor_->publish_state(line);
+  }
+
+  void set_text_sensor(text_sensor::TextSensor *text_sensor) {
+    this->text_sensor_ = text_sensor;
+  }
+
  protected:
-  std::string line_;
+  text_sensor::TextSensor *text_sensor_;
 };
+
+}  // namespace uart_read_line_sensor
+}  // namespace esphome
